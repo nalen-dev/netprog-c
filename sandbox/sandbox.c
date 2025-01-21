7d34465dd1a1ecaaa348e57db52fecc387071166
nalendev <nalendrapraja11@gmail.com>
2025-01-21 23:20:43 +0800

    clear sandbox

--- sandbox/sandbox.c
+++ sandbox/sandbox.c
@@ -4,13 +4,5 @@
 #include <time.h>
 
 int main(){
-    int a = 2;
-   
-    time_t timer;
-    time(&timer);
-
-    printf("%ld\n",timer);
-    printf("Local time is: %s", ctime(&timer));
-    
-    return 0;
+ 
 }
